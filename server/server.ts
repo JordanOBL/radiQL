85fa6f5c8f96b7870cb3a949dacdc19bca93ea97
Jordan Williams <jordan.williams@codemith.io>
2023-03-04 00:59:32 -0800

    cors

--- server/server.ts
+++ server/server.ts
@@ -2,38 +2,41 @@ import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import router from './router';
 import dotenv from 'dotenv';
+import router from './router';
+
 dotenv.config();
 const port = process.env.PORT || 3000;
 export const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-if (process.env.NODE_ENV === 'production')
-  app.use(express.static(path.resolve(__dirname, '../dist')));
-else app.use(express.static(path.resolve(__dirname, '../client')));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+if (process.env.NODE_ENV === 'production') { app.use(express.static(path.resolve(__dirname, '../dist'))); } else app.use(express.static(path.resolve(__dirname, '../client')));
 
 // all routes go through the router
 app.use('/', router); 
 
 // error handler
-app.use((err: {log: string, status: number, message: { err: string }}, req: Request, res: Response, next: NextFunction) => {
-const defaultErr = {
-  log: 'Express error handler caught unknown middleware error',
-  status: 500,
-  message: { err: 'An error occurred' },
-};
-	const errorObj = Object.assign({}, defaultErr, err);
-	console.log(errorObj.log);
+app.use((err: {log: string, status: number, message: { err: string }}, req: Request, res: Response) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = { ...defaultErr, ...err };
+  console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-
 app.listen(port, () => {
-	console.log('listening on port', port);
+  console.log('listening on port', port);
 });
